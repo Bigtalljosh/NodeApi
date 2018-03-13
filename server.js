b7259da7b2b81f1aedc3fd163f1a618ee5e4b4df
Josh Dadak <joshdadak@gmail.com>
2018-03-13 21:59:20 +0000

    Added Create and Get methods

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-
+const db = require('./config/db');
 const app = express();
-
 const port = 8000;
 
-app.listen(port, () => {
-    console.log("Live on : " + port);
+app.use(bodyParser.urlencoded({extended: true}));
+
+MongoClient.connect(db.url, (err, database) => {
+    if(err) return console.log(err);
+
+    const notesDb = database.db('notesdb');
+    require('./app/routes')(app, notesDb);
+
+    app.listen(port, () => {
+        console.log("Live on : " + port);
+    });
 });
